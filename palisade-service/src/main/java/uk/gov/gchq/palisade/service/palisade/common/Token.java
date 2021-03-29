9cc4d9cfb9562095e3e538de1b6b1e4660be0afe
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2021-03-29 11:41:36 +0100

    PAL-999, updated Palisade Service to use inline common code

--- palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/common/Token.java
+++ palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/common/Token.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.gov.gchq.palisade.service.palisade.model;
+package uk.gov.gchq.palisade.service.palisade.common;
 
 /**
  * Simply stores the expected header key for Tokens
